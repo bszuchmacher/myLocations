7b188d7e2db391234b9f7d958cdf044a4fe90966
Benjamin Szuchmacher <benszuchmacher@Benjamins-MacBook-Pro.local>
2021-02-23 20:41:33 +0200

    super small fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,7 @@ render(){
           <h1 style={{fontSize:"50px",textDecorationLine: "underline"}}>Welcome to Ben's Location's Page!</h1>
           <h2 style={{fontSize:"30px",textDecorationLine: "underline",marginTop:"3%" }}>Categories & Locations</h2>
           <p style={{fontSize:"20px",marginTop:"2%"}}>The app consists of 2 types of entities: <b>CATEGORIES and LOCATIONS</b> <br></br>While CATEGORY is a <u>string</u>, LOCATION is an <u>object</u> <b><u>with string properties</u></b>:<br></br> NAME, CATEGORY, ADDRESS & COORDINATES</p>
-          <p style={{fontSize:"18px"}}>To navigate between <b> Categories and Locations </b> -- use the Footer Navbar <b>(DOWN BELOW)</b></p>
+          <p style={{fontSize:"18px"}}>To navigate between <b> Categories and Locations </b> -- use the Footer Navbar <b>(ON THE SIDE BAR)</b></p>
           <h2 style={{fontSize:"30px",textDecorationLine: "underline", marginTop:"3%"}} >Toolbar</h2>
           <p style={{fontSize:"18px"}}>My <b>Toolbar</b> contains <b>4</b> operations: <b>View, Add, Delete, Update </b>- each available for both entity types</p>
         </div>
